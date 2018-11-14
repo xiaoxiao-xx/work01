055ea806652e969412c2f732aa0ccec20806946c
Lesiele Zhang <zhangqiangao@sina.cn>
2018-11-14 13:10:37 +0800

    lesileZhang 006

---
+++ src/main/webapp/js/importUser.js
@@ -0,0 +1,21 @@
+$("#btn_add").click(function () {
+    $("#myModalLabel").text("新增");
+    $('#myModal').modal();
+});
+
+$("#btn_submit").click(function () {
+    alert("ssss")
+   /* $.ajax({
+        url: '/importUser.ajax',
+        type: 'post',
+        data:{
+            keyword:keyword
+        },
+        success: function (res) {
+            alert("添加成功")
+        },
+        error: function () {
+
+        }
+    });*/
+});
